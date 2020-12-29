fea781ef96f990bea817510e7a676c69182a87d1
Mohammed Shehata <mshehata@identity-solutions.org>
2020-12-29 16:42:28 +0200

    UPDATE setup action bar with navigation controller

--- app/src/main/java/com/sh/nytimes/ui/activities/MainActivity.kt
+++ app/src/main/java/com/sh/nytimes/ui/activities/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.sh.nytimes.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.sh.nytimes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpActionBar()
+    }
+
+    private fun setUpActionBar() {
+
+        val navHostFragment =
+                supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val appBarConfiguration = AppBarConfiguration(navHostFragment.navController.graph)
+        setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
     }
 }
