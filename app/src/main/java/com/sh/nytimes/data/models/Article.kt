d926264f7c276491541dc63b092f43fadc40a28d
Mohammed Shehata <mshehata@identity-solutions.org>
2020-12-29 20:25:14 +0200

    UPDATE make models to implement parcelable

--- app/src/main/java/com/sh/nytimes/data/models/Article.kt
+++ app/src/main/java/com/sh/nytimes/data/models/Article.kt
@@ -1,5 +1,7 @@
 package com.sh.nytimes.data.models
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 data class Article(
@@ -19,4 +21,53 @@ data class Article(
     val updated: String,
     val uri: String,
     val url: String
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readLong(),
+        parcel.createTypedArrayList(Media)!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(abstractDescription)
+        parcel.writeString(adxKeywords)
+        parcel.writeString(byline)
+        parcel.writeLong(id)
+        parcel.writeTypedList(media)
+        parcel.writeString(publishedDate)
+        parcel.writeString(section)
+        parcel.writeString(source)
+        parcel.writeString(subsection)
+        parcel.writeString(title)
+        parcel.writeString(type)
+        parcel.writeString(updated)
+        parcel.writeString(uri)
+        parcel.writeString(url)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Article> {
+        override fun createFromParcel(parcel: Parcel): Article {
+            return Article(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Article?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/java/com/sh/nytimes/data/models/Media.kt
+++ app/src/main/java/com/sh/nytimes/data/models/Media.kt
@@ -1,5 +1,7 @@
 package com.sh.nytimes.data.models
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 data class Media(
@@ -9,4 +11,35 @@ data class Media(
     val metadata: List<MediaMetadata>,
     val subtype: String,
     val type: String
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.createTypedArrayList(MediaMetadata)!!,
+        parcel.readString()!!,
+        parcel.readString()!!
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(caption)
+        parcel.writeString(copyright)
+        parcel.writeTypedList(metadata)
+        parcel.writeString(subtype)
+        parcel.writeString(type)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Media> {
+        override fun createFromParcel(parcel: Parcel): Media {
+            return Media(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Media?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
