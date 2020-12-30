38c2d179faa852b92d5ea82a08aaf7bdaf52f33d
Mohammed Shehata <mshehata@identity-solutions.org>
2020-12-30 03:21:52 +0200

    FIX add toolbar to sync it with navigation controller

--- app/src/main/java/com/sh/nytimes/ui/activities/MainActivity.kt
+++ app/src/main/java/com/sh/nytimes/ui/activities/MainActivity.kt
@@ -2,9 +2,9 @@ package com.sh.nytimes.ui.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.sh.nytimes.R
 
 class MainActivity : AppCompatActivity() {
@@ -17,9 +17,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun setUpActionBar() {
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val appBarConfiguration = AppBarConfiguration(navHostFragment.navController.graph)
-        setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
+        toolbar.setupWithNavController(navHostFragment.navController)
     }
 }
